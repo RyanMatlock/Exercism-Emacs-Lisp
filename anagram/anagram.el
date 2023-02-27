45485b2a4fd35917a06a292287b6a2a1e525d436
Ryan Matlock <ryan.matlock@gmail.com>
2023-02-27 15:46:53 -0800

    :construction: define helper functions for anagrams-for

--- anagram/anagram.el
+++ anagram/anagram.el
@@ -2,9 +2,25 @@
 
 ;;; Commentary:
 
+;; how to form a cons cell
+;; (message (format "%s" (cons "a" 2)))  ;; (a . 2)
+
 (defun anagrams-for (subject candidates)
-;;; Code:
-)
+  "Determine which, if any, of the words in candidates are anagrams of
+subject."
+  (defun count-letters (word)
+    "Counts instances of each letter in a given word, e.g. apple:
+'((a . 1)
+  (e . 1)
+  (l . 2
+  (p . 2))
+[note: sorting the list alphabetically should make comparision easier]"
+    )
+  (defun anagrams-for-helper (subject candidates anagrams)
+    (cond ((not candidates) anagrams)
+          ((string= (downcase subject) (cons (downcase candidates)))
+           (anagrams-for-helper subject (car candidates) anagrams))))
+  (anagrams-for-helper subjects candidates '()))
 
 (provide 'anagram)
 ;;; anagram.el ends here
