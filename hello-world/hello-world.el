41d7d915b79b0b4f5d98d38b292ea9f3d9eafbe6
Ryan Matlock <ryan.matlock@gmail.com>
2023-02-27 12:30:52 -0800

    :tada: start exercise hello-world

---
+++ hello-world/hello-world.el
@@ -0,0 +1,9 @@
+;;; hello-world.el --- Hello World exercise (exercism)  -*- lexical-binding: t; -*-
+
+;;; Commentary:
+
+(defun hello ()
+  "Goodbye, Mars!")
+
+(provide 'hello-world)
+;;; hello-world.el ends here
