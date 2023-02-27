22342f59a404a2c9bb457d58980f5ac80d80e272
Ryan Matlock <ryan.matlock@gmail.com>
2023-02-27 14:11:51 -0800

    :construction: implement failing solution

--- two-fer/two-fer.el
+++ two-fer/two-fer.el
@@ -3,8 +3,13 @@
 ;;; Commentary:
 
 (defun two-fer (&optional name)
-;;; Code:
-)
+  "return string 'One for <name>, one for me.' where <name> is 'you' if not
+   passed."
+  (let ((two-fer-string (lambda (n)
+                         (format "One for %s, one for me." n))))
+    (if name
+        (two-fer-string name)
+      (two-fer-string "you"))))
 
 (provide 'two-fer)
 ;;; two-fer.el ends here
