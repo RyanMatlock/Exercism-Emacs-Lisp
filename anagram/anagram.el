8344d29b12adbe5f6ade82a79b9e620c47adc714
Ryan Matlock <ryan.matlock@gmail.com>
2023-02-27 15:11:27 -0800

    :tada: start exercise anagram

---
+++ anagram/anagram.el
@@ -0,0 +1,10 @@
+;;; anagram.el --- Anagram (exercism)  -*- lexical-binding: t; -*-
+
+;;; Commentary:
+
+(defun anagrams-for (subject candidates)
+;;; Code:
+)
+
+(provide 'anagram)
+;;; anagram.el ends here
