87fe275d186f5ec8f07c6fbfb43b5a8f4024c572
Ryan Matlock <ryan.matlock@gmail.com>
2023-02-28 10:53:06 -0800

    :construction: take a simpler approach to helper functions
    No need to use alists for this problem; ordered lists of letters should do just
    fine.

--- anagram/anagram.el
+++ anagram/anagram.el
@@ -2,62 +2,52 @@
 
 ;;; Commentary:
 
-;; how to form a cons cell
-;; (message (format "%s" (cons "a" 2)))  ;; (a . 2)
+;; (word-to-letters-signature "stop")
+;; ("o" "p" "s" "t")
+;; (word-to-letters-signature "pots")
+;; ("o" "p" "s" "t")
+;; (defun w2s (x) (sort (mapcar #'identity (downcase x))
+;;                             '<))
+;; w2s
+;; (w2s "stop")
+;; (111 112 115 116)
 
-;; (message (format "car of nil: '%s'" (car '())))  ;; nil
-
-;; (message (format "car of 'apple': '%s'" (car "apple")))
-;; format: Wrong type argument: listp, "apple"
-;; you need to start using 「M-x ielm」 to test out Emacs Lisp
-
-;; https://www.gnu.org/software/emacs/manual/html_node/elisp/Sequences-Arrays-Vectors.html
-;; strings are a subset of arrays, and lists and arrays are a subset of
-;; sequences
-
-;; (mapcar #'identity "apple")
-;; (97 112 112 108 101)
-;; (mapcar #'string '(97 112 112 108 101))
-;; ("a" "p" "p" "l" "e")
-
-;; (sort '(("a" . 1) ("p" . 2) ("l" . 1) ("e" . 1)) '(lambda (x y)
-;;                                                            (< (car x) (car y))))
-;; *** Eval error ***  Wrong type argument: number-or-marker-p, "p"
-
-;; (print (sort '(("a" . 1) ("p" . 2) ("l" . 1) ("e" . 1))
-;;                     '(lambda (x y)
-;;                        (string< (car x) (car y)))))
-
-;; (("a" . 1) ("e" . 1) ("l" . 1) ("p" . 2))
-
-;; (print (sort-alist '(("a" . 1) ("p" . 2) ("l" . 1) ("e" . 1))
-;;                           'string<))
-
-;; (("a" . 1) ("e" . 1) ("l" . 1) ("p" . 2))
+;; ELISP> (not '())
+;; t
+;; ELISP> (not '("a" "b"))
+;; nil
+;; ELISP> (and (not '()) (not '()))
+;; t
+;; ELISP> (and (not '()) (not '("a" "b")))
+;; nil
 
 (defun anagrams-for (subject candidates)
   "Determine which, if any, of the words in candidates are anagrams of
 subject."
-  ;; seems like something like this should be built in, but I couldn't find it
-  (defun string-to-letters (str)
-    "Example: convert 'apple' to ('a' 'p' 'p' 'l' 'e')."
-    (mapcar #'string (mapcar #'identity str)))
-  (defun sort-alist (alist comparison)
-    "Sort association list using function comparison (e.g. for alists with keys
-of type string, you'd pass string< for an ascending list)."
-    (sort alist #'(lambda (x y)
-                    (funcall comparison (car x) (car y)))))
-  (defun count-letters (word)
-    "Counts instances of each letter in a given word and returns an association
-list, e.g. apple:
-'((a . 1)
-  (e . 1)
-  (l . 2
-  (p . 2))
-[note: sorting the list alphabetically should make comparision easier]"
-    (let* ((lcase-word (downcase word))
-           (letters ))
-      ))
+  (defun word-to-letters-signature (str)
+    "Convert a word to an alphabetically ascending list of letters, including
+repeats.
+
+Words that are anagrams should possess the same ordered letter list,
+e.g. 'stop' and 'pots' should both produce ('o' 'p' 's' 't')."
+    ;; technically, the outer mapcar isn't necessary, but the output is chars
+    ;; AKA ints, so converting them to strings makes debugging easier
+    (sort (mapcar #'string (mapcar #'identity (downcase str)))
+          'string<))
+  (defun identical-letter-list-p (xs ys)
+    "Determine if two ordered lists of lowercase letters are identical."
+    ;; there's probably a cleverer way of doing this with and and/or or
+    ;; functions and omitting the cond entirely
+    (cond
+     ;; if xs and ys are empty, then they're the same
+     ((and (not xs) (not ys)) t
+      ;; redundant
+      ;; ;; if xs and ys are different lengths, they're not the same
+      ;; ((not (eq (length xs) (length ys))) nil)
+      ;; if the first element is different, they're not the same
+      ((not (string= (car xs) (car ys))) nil)
+      ;; recursive step: check next eleemnt
+      (t identical-letter-list-p (cdr xs) (cdr ys)))))
   (defun anagrams-for-helper (subject candidates anagrams)
     "Build a list of anagrams of subject from candidates."
     ;; (let ((dsubject (downcase subject))
