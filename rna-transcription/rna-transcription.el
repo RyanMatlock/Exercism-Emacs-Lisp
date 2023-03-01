6cb366153783fa6bf0b6cd8528d1116204f7e2b7
Ryan Matlock <ryan.matlock@gmail.com>
2023-03-01 13:47:02 -0800

    :sparkles: create function to transcribe individual nucleotides

--- rna-transcription/rna-transcription.el
+++ rna-transcription/rna-transcription.el
@@ -3,7 +3,18 @@
 ;;; Commentary:
 
 (defun to-rna (strand)
-;;; Code:
+  "Convert sequence of DNA (as a string) to a sequence of RNA (also a string)."
+  (defun dna-nucleotide-to-rna (dna-nucleotide)
+    "Convert single DNA dna-nucleotide to RNA dna-nucleotide as follows:
+`G` -> `C`
+`C` -> `G`
+`T` -> `A`
+`A` -> `U`"
+    (cond ((string= dna-nucleotide "G") "C")
+          ((string= dna-nucleotide "C") "G")
+          ((string= dna-nucleotide "T") "A")
+          ((string= dna-nucleotide "A") "U")
+          (t (error "Encountered unknown DNA nucleotide."))))
 )
 
 (provide 'rna-transcription)
