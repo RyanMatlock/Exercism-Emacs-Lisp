09f7ec8e376aa4d72c77f24989d8725977299e37
Ryan Matlock <ryan.matlock@gmail.com>
2023-03-01 14:19:48 -0800

    :rocket: finish exercise rna-transcription

--- rna-transcription/rna-transcription.el
+++ rna-transcription/rna-transcription.el
@@ -9,13 +9,28 @@
 `G` -> `C`
 `C` -> `G`
 `T` -> `A`
-`A` -> `U`"
+`A` -> `U`
+otherwise -> error
+
+Note: this will throw an error for lowercase inputs."
     (cond ((string= dna-nucleotide "G") "C")
           ((string= dna-nucleotide "C") "G")
           ((string= dna-nucleotide "T") "A")
           ((string= dna-nucleotide "A") "U")
           (t (error "Encountered unknown DNA nucleotide."))))
-)
+  (let* ((dna-sequence (mapcar #'string strand))
+         (rna-sequence (mapcar #'dna-nucleotide-to-rna dna-sequence)))
+    (mapconcat #'identity rna-sequence "")))
+
+;; -- IELM testing --
+;; ELISP> (mapcar #'string "ACTG")
+;; ("A" "C" "T" "G")
+;; ELISP> (mapcar #'dna-nucleotide-to-rna '("A" "C" "T" "G"))
+;; ("U" "G" "A" "C")
+;; ELISP> (mapconcat #'identity '("U" "G" "A" "C") "")
+;; "UGAC"
+;; ELISP> (to-rna "ACTG")
+;; "UGAC"
 
 (provide 'rna-transcription)
 ;;; rna-transcription.el ends here
