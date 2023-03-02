306e6efd669667ffb0a2cc4a38c067bd931d3626
Ryan Matlock <ryan.matlock@gmail.com>
2023-03-02 11:00:07 -0800

    :construction: define lookup function for non-compound roman numerals

--- roman-numerals/roman-numerals.el
+++ roman-numerals/roman-numerals.el
@@ -3,7 +3,17 @@
 ;;; Commentary:
 
 (defun to-roman (value)
-;;; Code:
+  (defun simple-roman-lookup (simple-number)
+    "Look up non-compound Roman numerals."
+    (let ((roman-numeral-alist '((0 . "")
+                                 (1 . "I")
+                                 (5 . "V")
+                                 (10 . "X")
+                                 (50 . "L")
+                                 (100 . "C")
+                                 (500 . "D")
+                                 (1000 . "M"))))
+      (cdr (assoc simple-number roman-numeral-alist))))
 )
 
 (provide 'roman-numerals)
