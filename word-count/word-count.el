f9e7689f5aa4ab1a48b61afe4c757aecb0e33702
Ryan Matlock <ryan.matlock@gmail.com>
2023-03-02 13:30:37 -0800

    :tada: start exercise word-count

---
+++ word-count/word-count.el
@@ -0,0 +1,10 @@
+;;; word-count.el --- word-count Exercise (exercism)  -*- lexical-binding: t; -*-
+
+;;; Commentary:
+
+(defun word-count (sentence)
+;;; Code:
+)
+
+(provide 'word-count)
+;;; word-count.el ends here
