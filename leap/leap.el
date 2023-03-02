df027f3c994c947148af245d4b8175c655e5a53c
Ryan Matlock <ryan.matlock@gmail.com>
2023-03-01 16:04:12 -0800

    :rocket: finish exercise leap

--- leap/leap.el
+++ leap/leap.el
@@ -3,8 +3,20 @@
 ;;; Commentary:
 
 (defun leap-year-p (year)
-;;; Code:
-)
+  "Determine if a year is a leap year."
+  (defun divisible-by-p (number divisor)
+    "Determine if number is divisible by divisor."
+    (eq (mod number divisor) 0))
+  (cond ((divisible-by-p year 400) t)
+        ((divisible-by-p year 100) nil)
+        ((divisible-by-p year 4) t)
+        (t nil)))
+
+;; -- IELM testing --
+;; ELISP> (divisible-by-p 21 7)
+;; t
+;; ELISP> (divisible-by-p 22 7)
+;; nil
 
 (provide 'leap-year-p)
 ;;; leap.el ends here
