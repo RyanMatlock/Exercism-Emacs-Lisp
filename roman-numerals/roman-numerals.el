90f16c7e5f4fe6c01c48df7f08dcce7418a855c0
Ryan Matlock <ryan.matlock@gmail.com>
2023-03-02 13:24:55 -0800

    :construction: break number into "Roman OOM" and multiplier
    Might be the wrong direction to go given that 9 doesn't get broken up in an
    easy way to handle; might be easier to do normal order of magnitude breakdown
    and work from there

--- roman-numerals/roman-numerals.el
+++ roman-numerals/roman-numerals.el
@@ -3,18 +3,46 @@
 ;;; Commentary:
 
 (defun to-roman (value)
-  (defun simple-roman-lookup (simple-number)
-    "Look up non-compound Roman numerals."
-    (let ((roman-numeral-alist '((0 . "")
-                                 (1 . "I")
-                                 (5 . "V")
-                                 (10 . "X")
-                                 (50 . "L")
-                                 (100 . "C")
-                                 (500 . "D")
-                                 (1000 . "M"))))
-      (cdr (assoc simple-number roman-numeral-alist))))
-)
+  (let* ((roman-numeral-alist '((0 . "")
+                                (1 . "I")
+                                (5 . "V")
+                                (10 . "X")
+                                (50 . "L")
+                                (100 . "C")
+                                (500 . "D")
+                                (1000 . "M")))
+         ;; stolen from https://emacs.stackexchange.com/a/33626 when looking
+         ;; for a standard function for this operation
+         ;; I can skip the first element
+         (roman-base-values (mapcar #'car (cdr roman-numeral-alist))))
+    (defun simple-roman-lookup (simple-number)
+      "Look up non-compound Roman numerals."
+      (alist-get simple-number roman-numeral-alist))
+    (defun roman-oom-and-multple (n)
+      "Calculate the Roman numeral 'order of magnitude,' which goes in 5s
+instead of 10s, as well as the multple of that OOM and return it as an alist in
+the form (OOM . multiple)"
+      ;; I need the base values in descending order because I need to pick off
+      ;; the biggest chunks first
+      (let ((base-values (reverse roman-base-values)))
+        (defun oom-mult-helper (n base-values)
+          (let* ((oom (car base-values))
+                 (quotient (/ n oom)))
+            (if (> quotient 0)
+                (cons oom quotient)
+              (oom-mult-helper n (cdr base-values)))))
+        (oom-mult-helper n base-values))))
+  )
+
+;; -- IELM testing --
+;; ELISP> (simple-roman-lookup 500)
+;; "D"
+;; ELISP> (roman-oom-and-multple 49)
+;; (10 . 4)
+;; ELISP> (roman-oom-and-multple 9)
+;; (5 . 1)
+;; ELISP> (roman-oom-and-multple 4)
+;; (1 . 4)
 
 (provide 'roman-numerals)
 ;;; roman-numerals.el ends here
