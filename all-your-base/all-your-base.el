97d72c5cb7c03fd9ad3294acb338a11e878b5796
Ryan Matlock <ryan.matlock@gmail.com>
2023-03-01 23:53:35 -0800

    :construction: write helper function for base10 to base-N conversion

--- all-your-base/all-your-base.el
+++ all-your-base/all-your-base.el
@@ -5,6 +5,17 @@
 ;;; Code:
 
 (defun rebase (list-of-digits in-base out-base)
+  (defun base10-to-base-n (base10-digits n accumulator)
+    "Convert digits in base-10 to base-n"
+    (if base10-digits
+        (let ((power (1- (length base10-digits)))
+              (x (car base10-digits)))
+          ;; recursive step
+          (base10-to-base-n
+           (cdr base10-digits)
+           n
+           (cons (* x (expt n power)) accumulator)))
+      (apply #'+ accumulator)))
   (error "Delete this S-Expression and write your own implementation"))
 
 (provide 'all-your-base)
