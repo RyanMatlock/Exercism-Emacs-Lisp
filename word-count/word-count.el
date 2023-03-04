3d6d02c0a9c0a136315321e759304f2a30d56d40
Ryan Matlock <ryan.matlock@gmail.com>
2023-03-04 15:31:01 -0800

    :construction: write function that match tests' type expectations :face_palm:

--- word-count/word-count.el
+++ word-count/word-count.el
@@ -27,7 +27,9 @@ initial value of 1."
         (mapconcat #'identity (reverse acc) "\n"))))
   (defun wc-helper (words wc-alist)
     (let ((word (car words)))
-      (if word
+      ;; need to check that word isn't the empty string in case the sentence
+      ;; passed to word-count is empty
+      (if (and word (not (string= word "")))
           (wc-helper (cdr words) (alist-increment-value word wc-alist))
         ;; to get the original sentence order
         (reverse wc-alist))))
@@ -44,7 +46,8 @@ initial value of 1."
       "^[[:punct:]]+" "" word)))
   (let* ((words (mapcar #'downcase (split-string sentence "[[:blank:]\n]+")))
          (processed-words (mapcar #'strip-punctuation-from-word words)))
-    (wc-alist-formatter (wc-helper processed-words '()))))
+    ;; (wc-helper processed-words '())
+    (wc-alist-formatter (wc-helper processed-words '()) '())))
 
 ;; -- IELM testing --
 ;; ELISP> (wc-alist-formatter '(("foo" . 2) ("bar". 3)))
@@ -137,6 +140,98 @@ initial value of 1."
 ;; though?") t) (wc-alist acc) (let ((entry (car wc-alist))) (if entry (let ((formatted-entry (format "%s: %d" (car entry) (cdr entry)))) (wc-alist-formatter (cdr wc-alist) (cons formatted-entry acc))) (mapconcat #'identity (reverse acc) "
 ;; ")))), 1
 ;; have fun debugging that
+
+;; ELISP> (wc-helper '("foo" "foo" "bar") '())
+;; (("foo" . 2)
+;;  ("bar" . 1))
+
+;; ELISP> (wc-alist-formatter (wc-helper '("foo" "foo" "bar") '()))
+;; *** Eval error ***  Wrong number of arguments: ((t) (wc-alist acc) (let ((entry (car wc-alist))) (if entry (let ((formatted-entry (format "%s: %d" (car entry) (cdr entry)))) (wc-alist-formatter (cdr wc-alist) (cons formatted-entry acc))) (mapconcat #'identity (reverse acc) "
+;; ")))), 1
+
+;; ELISP> (word-count "'Egads! My roast is ruined!' exclaimed Skinner. Is it
+;; though?")
+;; (("egads" . 1)
+;;  ("my" . 1)
+;;  ("roast" . 1)
+;;  ("is" . 2)
+;;  ("ruined" . 1)
+;;  ("exclaimed" . 1)
+;;  ("skinner" . 1)
+;;  ("it" . 1)
+;;  ("though" . 1))
+
+;; (wc-alist-formatter (wc-helper '("foo" "foo" "bar") '()) '())
+;; "foo: 2
+;; bar: 1"
+;; I forgot to call it with the initial accumulator value of '() 🤦
+
+;; ELISP> (word-count "'Egads! My roast is ruined!' exclaimed Skinner. Is it
+;; though?")
+;; "egads: 1
+;; my: 1
+;; roast: 1
+;; is: 2
+;; ruined: 1
+;; exclaimed: 1
+;; skinner: 1
+;; it: 1
+;; though: 1"
+
+;; ELISP> (word-count "\"That's the password: 'PASSWORD 123'!\", cried the Special Agent.\nSo I fled.")
+;; "that's: 1
+;; the: 2
+;; password: 2
+;; 123: 1
+;; cried: 1
+;; special: 1
+;; agent: 1
+;; so: 1
+;; i: 1
+;; fled: 1"
+
+;; looks like it's working, and it's even getting the words in the right order
+
+;; ok, failing all the tests, so let's start addressing these
+;; ELISP> (word-count "")
+;; ": 1"
+;; yeah, that's a problem
+
+;; ELISP> (word-count "")
+;; ""
+
+;;;;;;;;;;;;;;;;;;;;;;;;; ERT results ;;;;;;;;;;;;;;;;;;;;;;;;;
+;; FFFFFFFFF
+
+;; F count-one-of-each-word-test
+;;     (wrong-type-argument list-or-vector-p "one: 1\nof: 1\neach: 1")
+
+;; F count-one-word-test
+;;     (wrong-type-argument list-or-vector-p "word: 1")
+
+;; F ignore-punctuation-test
+;;     (wrong-type-argument list-or-vector-p "car: 1")
+
+;; F include-numbers-test
+;;     (wrong-type-argument list-or-vector-p "testing: 2\n1: 1\n2: 1")
+
+;; F multiple-occurrences-of-a-word-test
+;;     (wrong-type-argument list-or-vector-p "one: 1\nfish: 4\ntwo: 1\nred: 1\nblue: 1")
+
+;; F no-words-test
+;;     (wrong-type-argument list-or-vector-p "")
+
+;; F normalize-case-test
+;;     (wrong-type-argument list-or-vector-p "go: 3\nstop: 2")
+
+;; F quotation-for-word-with-apostrophe-test
+;;     (wrong-type-argument list-or-vector-p "can: 1\ncan't: 2")
+
+;; F with-apostrophes-test
+;;     (wrong-type-argument list-or-vector-p "first: 1\ndon't: 2\nlaugh: 1\nthen: 1\ncry: 1\nyou're: 1\ngetting: 1\nit: 1")
+
+;;;; look at what the tests are telling you: you're returning the wrong type;
+;;;; don't go by what the Exercism website's toy example; read the tests first!
 
 (provide 'word-count)
 ;;; word-count.el ends here
