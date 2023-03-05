1cd5eb801b6f4300459766245eebdc0843bbcb6d
Ryan Matlock <ryan.matlock@gmail.com>
2023-03-04 19:57:06 -0800

    :tada: start exercise allergies

---
+++ allergies/allergies.el
@@ -0,0 +1,16 @@
+;;; allergies.el --- Allergies Exercise (exercism)  -*- lexical-binding: t; -*-
+
+;;; Commentary:
+
+(defun allergen-list (score)
+"List all allergens with a given SCORE."
+;;; Code:
+)
+
+(defun allergic-to-p (score allergen)
+"Check if Allergic to allergen based on SCORE and ALLERGEN."
+;;; Code:
+)
+
+(provide 'allergies)
+;;; allergies.el ends here
