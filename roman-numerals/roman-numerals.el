46846181e650668326de66d4d61f1069e64b2f8a
Ryan Matlock <ryan.matlock@gmail.com>
2023-03-06 17:28:48 -0800

    :construction: over-complicate everything; pass 2/25 tests

--- roman-numerals/roman-numerals.el
+++ roman-numerals/roman-numerals.el
@@ -18,6 +18,46 @@
     (defun simple-roman-lookup (simple-number)
       "Look up non-compound Roman numerals."
       (alist-get simple-number roman-numeral-alist))
+    ;; (defun even-p (n)
+    ;;   "Determine if n is an even number."
+    ;;   (eq 0 (mod n 2)))
+    ;; (defun odd-p (n)
+    ;;   "Determine if n is an odd number."
+    ;;   (not (even-p n)))
+    ;; (defun 5s-oom-p (oom)
+    ;;   "Determine if ")
+    (defun position-in-list (element list &optional test)
+      "Return position in list of first occurrence of element starting at 0 (as
+god intended; seriously, Lua, MATLAB, etc., what is wrong with you?); if
+element is not in list, return nil. test is the function used to compare
+element and the elements of list (default: eq).
+
+  (nth (position-in-list element list) list)
+
+should return element if element is in list."
+      ;; see https://emacs.stackexchange.com/a/14203
+      (let ((test (or test #'eq)))
+        (defun pos-in-list-helper (e lst pos)
+          (let ((first (car lst)))
+            (if first
+                ;; I always forget funcall
+                (if (funcall test e first)
+                    pos
+                  (pos-in-list-helper e (cdr lst) (1+ pos)))
+              nil)))
+        (pos-in-list-helper element list 0)))
+    (defun prev-roman-oom (roman-oom)
+      "Return the next-lowest roman-oom (returns nil if roman-oom is 1)."
+      )
+    (defun 10s-oom-p (roman-oom)
+      "Determine if roman-oom (order of magnitude) is in the 10s series (for
+lack of a better term), i.e. 1, 10, 100, 1000, rather than the 5s series, i.e.
+5, 50, 500.
+
+This may be helpful in handling the case of 9s."
+      (and
+       ;; do stuff
+       t t))
     (defun roman-oom-and-multple (n)
       "Calculate the Roman numeral 'order of magnitude,' which goes in 5s
 instead of 10s, as well as the multple of that OOM and return it as an alist in
@@ -44,7 +84,7 @@ the form (OOM . multiple)"
              ((> quotient 0) (cons oom quotient))
              (t (oom-mult-helper n (cdr base-values))))))
         (oom-mult-helper n base-values))))
-  )
+  (simple-roman-lookup value))
 
 ;; -- IELM testing --
 ;; ELISP> (simple-roman-lookup 500)
@@ -57,6 +97,18 @@ the form (OOM . multiple)"
 ;; (1 . 4)
 ;; this might not be the right strategy given the way 9 gets broken up
 ;; on second thought, what if I had a special case for 9?
+
+;; ELISP> (position-in-list 3 '(1 2 3 4))
+;; 2 (#o2, #x2, ?\C-b)
+;; ELISP> (position-in-list "bar" '("foo" "bar" "baz") #'string=)
+;; 1 (#o1, #x1, ?\C-a)
+;; ELISP> (position-in-list "quux" '("foo" "bar" "baz") #'string=)
+;; nil
+;; ELISP> (let ((lst '("foo" "bar" "baz")))
+;; (nth (position-in-list "bar" lst #'string=) lst))
+;; "bar"
+
+;; nice
 
 (provide 'roman-numerals)
 ;;; roman-numerals.el ends here
