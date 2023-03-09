aafffd12bf0ab155207a3e8e0167920c8de37792
Ryan Matlock <ryan.matlock@gmail.com>
2023-03-09 10:01:39 -0800

    :construction: write function to take a slice of a list

--- sublist/sublist.el
+++ sublist/sublist.el
@@ -7,10 +7,28 @@
 (defun list-classify (list1 list2)
   "Determine if list1 is equal to, a sublist of, a superlist of, or unequal to
 list2."
+
+  (defun slice (xs start size)
+    "Create a sublist of xs beginning at index start of length size. Throw an
+error if trying to take a sublist that's too long.
+
+(slice xs 0 (length xs)) should return xs."
+    (if (< (- (length xs) start) size)
+        (error "Slice is too large.")
+      (defun slice-helper (xs start size result)
+        (let ((x (nth start xs)))
+          (if (> size 0)
+              (slice-helper xs (1+ start) (1- size) (cons x result))
+            (reverse result))))
+      (slice-helper xs start size '())))
+
   (defun compare-equal-length-lists (xs ys)
     "Determine if lists of equal length have identical elements in the same
 order, in which case they're :equal; otherwise, return :unequal."
     (cond ((eq (length xs) (length ys))
+           ;; only reason I'm writing the helper function is so the length
+           ;; check only happens once (not that performance really matters
+           ;; here)
            (defun compare-equal-length-lists-helper (xs ys)
              (let ((x (car xs))
                    (y (car ys)))
@@ -22,6 +40,7 @@ order, in which case they're :equal; otherwise, return :unequal."
                      (t :equal)))))
           (t (error "Lists are of unequal length.")))
     (compare-equal-length-lists-helper xs ys))
+
   (cond ((and (not list1) list2) :sublist)
         ((and list1 (not list2)) :superlist)
         ((eq (length list1) (length list2))
@@ -34,6 +53,31 @@ order, in which case they're :equal; otherwise, return :unequal."
 ;; ELISP> (compare-equal-length-lists '(1 2 3) '(1 2 3))
 ;; :equal
 
+;; ELISP> (slice '(1 2 3 4 5) 1 3)
+;; (2 3 4)
+
+;; ELISP> (slice '(1 2 3 4 5) 2 3)
+;; (3 4 5)
+
+;; ELISP> (slice '(1 2 3 4 5) 1 5)
+;; (2 3 4 5 nil)
+;; oops, going off the end
+
+;; ELISP> (slice '(1 2 3 4 5) 1 6)
+;; (2 3 4 5 nil nil)
+;; really going off the end!
+
+;; ELISP> (slice '(1 2 3 4 5) 1 7)
+;; *** Eval error ***  Slice is too large.
+
+;; ELISP> (slice '(1 2 3 4 5) 1 5)
+;; *** Eval error ***  Slice is too large.
+;; ELISP> (slice '(1 2 3 4 5) 1 4)
+;; (2 3 4 5)
+
+;; ELISP> (slice '(1 2 3 4 5) 0 5)
+;; (1 2 3 4 5)
+;; fixed
 
 (provide 'sublist)
 ;;; sublist.el ends here
