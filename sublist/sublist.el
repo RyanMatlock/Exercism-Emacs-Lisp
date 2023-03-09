5b68aa6a1b122ab17ce4d9f8078e5dd7994f5226
Ryan Matlock <ryan.matlock@gmail.com>
2023-03-09 04:30:17 -0800

    :construction: determine if equal length lists are equal or unequal

--- sublist/sublist.el
+++ sublist/sublist.el
@@ -5,7 +5,31 @@
 ;;; Code:
 
 (defun list-classify (list1 list2)
-  (error "Delete this S-Expression and write your own implementation"))
+  "Determine if list1 is equal to, a sublist of, a superlist of, or unequal to
+list2."
+  (defun compare-equal-length-lists (xs ys)
+    "Determine if lists of equal length have identical elements in the same
+order, in which case they're :equal; otherwise, return :unequal."
+    (cond ((eq (length xs) (length ys))
+           (defun compare-equal-length-lists-helper (xs ys)
+             (let ((x (car xs))
+                   (y (car ys)))
+               (cond ((and x y (eq x y))
+                      (compare-equal-length-lists-helper
+                       (cdr xs)
+                       (cdr ys)))
+                     ((not (eq x y)) :unequal)
+                     (t :equal)))))
+          (t (error "Lists are of unequal length.")))
+    (compare-equal-length-lists-helper xs ys))
+  (compare-equal-length-lists list1 list2))
+
+;; -- IELM testing --
+;; ELISP> (compare-equal-length-lists '("foo" "bar") '("bar" "foo"))
+;; :unequal
+;; ELISP> (compare-equal-length-lists '(1 2 3) '(1 2 3))
+;; :equal
+
 
 (provide 'sublist)
 ;;; sublist.el ends here
