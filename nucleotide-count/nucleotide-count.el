459389036939b11c9888db7d64d7030358d82e5e
Ryan Matlock <ryan.matlock@gmail.com>
2023-03-23 14:10:01 -0700

    :rocket: gather ideas together and finish the exercise

--- nucleotide-count/nucleotide-count.el
+++ nucleotide-count/nucleotide-count.el
@@ -3,8 +3,24 @@
 ;;; Commentary:
 
 (defun nucleotide-count (sequence)
-;;; Code:
-)
+  "Count number of instances of A, C, G, and T within SEQUENCE and report
+results as an alist."
+  (defun instances-of-char-in-string (c str)
+    "Count the number of times char C appears in string STR."
+    (length (seq-filter (lambda (letter) (eq c letter)) str)))
+  (let* ((nucleotides '(?A ?C ?G ?T))
+         (ncount-alist (mapcar
+                        #'(lambda (nucleotide)
+                            (cons
+                             nucleotide
+                             (instances-of-char-in-string
+                              nucleotide
+                              sequence)))
+                        nucleotides))
+         (num-nucleotides (apply #'+ (mapcar #'cdr ncount-alist))))
+    (if (length= sequence num-nucleotides)
+        ncount-alist
+      (error (format "Invalid nucleotide in '%s'." sequence)))))
 
 (provide 'nucleotide-count)
 ;;; nucleotide-count.el ends here
