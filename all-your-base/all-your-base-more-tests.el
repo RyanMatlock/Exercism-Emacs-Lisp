adcd230ba711d591641adbaf4a81d5b46c9610c8
Ryan Matlock <ryan.matlock@gmail.com>
2023-03-24 16:42:25 -0700

    :test_tube: write test for base 2 to base 10 conversion

---
+++ all-your-base/all-your-base-more-tests.el
@@ -0,0 +1,11 @@
+(load-file "all-your-base.el")
+
+(declare-function base-n-to-base-10
+                  "all-your-base.el"
+                  (list-of-digits in-base out-base))
+
+(ert-deftest base-2-to-base-10-42 ()
+  (should (equal 42 (base-n-to-base-10 '(1 0 1 0 1 0) 2))))
+
+(provide 'all-your-base-more-tests)
+
