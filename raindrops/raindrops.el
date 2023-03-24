1a7b232cbe4e7041206191ab44825dc7ff4f9e21
Ryan Matlock <ryan.matlock@gmail.com>
2023-03-24 13:34:50 -0700

    :tada: start exercise raindrops aka FizzBuzz

---
+++ raindrops/raindrops.el
@@ -0,0 +1,11 @@
+;;; raindrops.el --- Raindrops (exercism)  -*- lexical-binding: t; -*-
+
+;;; Commentary:
+
+(defun convert (n)
+  "Convert integer N to its raindrops string."
+;;; Code:
+)
+
+(provide 'raindrops)
+;;; raindrops.el ends here
