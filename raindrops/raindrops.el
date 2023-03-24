257f5e6be98f53c88b95a2f327a0c6b8e3161321
Ryan Matlock <ryan.matlock@gmail.com>
2023-03-24 14:36:43 -0700

    ✅ pass factorp tests

--- raindrops/raindrops.el
+++ raindrops/raindrops.el
@@ -1,11 +1,24 @@
 ;;; raindrops.el --- Raindrops (exercism)  -*- lexical-binding: t; -*-
 
-;;; Commentary:
+;;; Commentary: I'm (probably) overcomplicating this by using an alist, but I
+;;; want to solve this in such a way that if you wanted to add 20 more factors,
+;;; you'd only have to update the alist. Solving this with a cond and figuring
+;;; out all the permutation of factors is messier.
+
+  (defun factorp (num divisor)
+    "Return T if DIVISOR is a factor of NUM; otherwise return NIL; error if
+DIVISOR is 0."
+    (cond ((equal 0 divisor) (error "DIVISOR cannot be 0."))
+          (t (equal 0 (mod num divisor)))))
 
 (defun convert (n)
   "Convert integer N to its raindrops string."
-;;; Code:
-)
+  (let ((factors-alist '((3 . "Pling")
+                         (5 . "Plang")
+                         (7 . "Plong")))
+        (result ""))
+    )
+  )
 
 (provide 'raindrops)
 ;;; raindrops.el ends here
