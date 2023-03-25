75d3991988b68866f333af30c2a66d8639e050f5
Ryan Matlock <ryan.matlock@gmail.com>
2023-03-24 17:08:23 -0700

    :test_tube: create simple test for base 10 to base n conversion

--- all-your-base/all-your-base-more-tests.el
+++ all-your-base/all-your-base-more-tests.el
@@ -5,5 +5,10 @@
 (ert-deftest base-2-to-base-10-42 ()
   (should (equal 42 (base-n-to-base-10 '(1 0 1 0 1 0) 2))))
 
+(declare-function base-10-to-base-n "all-your-base.el" (base-10-num base-n))
+
+(ert-deftest base-10-to-base-2-42 ()
+  (should (equal '(1 0 1 0 1 0) (base-10-to-base-n (42 2)))))
+
 (provide 'all-your-base-more-tests)
 
