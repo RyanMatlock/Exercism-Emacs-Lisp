8498c33e47d4807f117c6995788dd810ccca4496
Ryan Matlock <ryan.matlock@gmail.com>
2023-03-24 17:08:03 -0700

    :adhesive_bandage: fix base-n-to-base-10 declaration

--- all-your-base/all-your-base-more-tests.el
+++ all-your-base/all-your-base-more-tests.el
@@ -1,8 +1,6 @@
 (load-file "all-your-base.el")
 
-(declare-function base-n-to-base-10
-                  "all-your-base.el"
-                  (list-of-digits in-base out-base))
+(declare-function base-n-to-base-10 "all-your-base.el" (digits base-n))
 
 (ert-deftest base-2-to-base-10-42 ()
   (should (equal 42 (base-n-to-base-10 '(1 0 1 0 1 0) 2))))
