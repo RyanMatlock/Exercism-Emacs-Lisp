e962e4c9d7200d87ef45b3f4b3e8c94f89353207
Ryan Matlock <ryan.matlock@gmail.com>
2023-03-26 12:55:56 -0700

    :tada: start exercise run-length-encoding

---
+++ run-length-encoding/run-length-encoding.el
@@ -0,0 +1,14 @@
+;;; run-length-encoding.el --- run-length-encoding Exercise (exercism)  -*- lexical-binding: t; -*-
+
+;;; Commentary:
+
+(defun run-length-encode (s)
+;;; Code:
+)
+
+(defun run-length-decode (s)
+;;; Code:
+)
+
+(provide 'run-length-encoding)
+;;; run-length-encoding.el ends here
