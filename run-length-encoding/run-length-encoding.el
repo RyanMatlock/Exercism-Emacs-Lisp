40aa2867bb3bf84102a6f17c9af3035ff4641bc2
Ryan Matlock <ryan.matlock@gmail.com>
2023-03-27 09:57:44 -0700

    :coffin: remove print-debug statements

--- run-length-encoding/run-length-encoding.el
+++ run-length-encoding/run-length-encoding.el
@@ -8,38 +8,25 @@ repeated consecutive characters with the number of repetitions followed by the
 character."
   (defun encode-helper (letters prev-letter count acc)
     (let* ((min-count 1)
-           (letter (car letters))
-           (print-debug (lambda (label)
-                          (print
-                           (format (concat "%s\n\t"
-                                           "letter: %s\t"
-                                           "prev-letter: %s\t"
-                                           "count: %d\t"
-                                           "acc: %s")
-                                   label letter prev-letter count acc)))))
+           (letter (car letters)))
       (cond ((and letter (string= letter prev-letter))
-             (funcall print-debug "string=:")
              (encode-helper (cdr letters) letter (1+ count) acc))
             ((> count min-count)
-             (funcall print-debug (format "count > %d:" min-count))
              (encode-helper
               (cdr letters)
               letter
               min-count
               (cons (format "%d%s" count prev-letter) acc)))
             ((and letter (not (string= "" prev-letter)))
-             (funcall print-debug "letter not nil and prev-letter not '':")
              (encode-helper
               (cdr letters)
               letter
               min-count
               (cons prev-letter acc)))
             (letter
-             (funcall print-debug "letter not nil")
              (encode-helper (cdr letters) letter min-count acc))
             (t
-             (funcall print-debug "out of letters")
-             ;; you need to stick the last letter on at the end
+             ;; stick the last letter on at the end, hence the cons
              (mapconcat #'identity (reverse (cons prev-letter acc)) "")))))
   (let ((slist (mapcar #'string s)))
     (encode-helper slist "" 1 '())))
