83ec3a3f417a47e7cbbdb17d3db648b44d66c8c2
Ryan Matlock <ryan.matlock@gmail.com>
2023-03-29 20:51:33 -0700

    ✅ pass run-length-decode tests

--- run-length-encoding/run-length-encoding.el
+++ run-length-encoding/run-length-encoding.el
@@ -49,8 +49,28 @@ string."
       (mapconcat #'identity (make-list n str) sep))))
 
 (defun run-length-decode (s)
-;;; Code:
-)
+  "Decode string S by repeating the character following a series of digits the
+number of times represented by those digits and return the decoded string."
+  (defun decode-helper (xs digits acc)
+    (let ((x (car xs))
+          (sep ""))
+      ;; there's some redudancy in these cond predicates -- if I were smarter,
+      ;; I'd have planned the order a little better and cut down on the number
+      ;; of checks
+      (cond ((str-digit-p x) (decode-helper (cdr xs) (cons x digits) acc))
+            ((and x digits (not (str-digit-p x)))
+             (let ((n-repeat
+                    (string-to-number
+                     (mapconcat #'identity (reverse digits) sep))))
+               (decode-helper
+                (cdr xs) '() (cons (repeat-str n-repeat x) acc))))
+            ((and x (str-digit-p x))
+             (decode-helper (cdr xs) (cons x digits) acc))
+            ((and x (not (str-digit-p x)))
+             (decode-helper (cdr xs) '() (cons x acc)))
+            (t (mapconcat #'identity (reverse acc) sep)))))
+  (let ((slist (mapcar #'string s)))
+    (decode-helper slist '() '())))
 
 (provide 'run-length-encoding)
 ;;; run-length-encoding.el ends here
