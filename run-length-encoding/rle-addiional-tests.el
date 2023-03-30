ad015d7dc2bd738f574ad6e8083fd4775fe4bb1b
Ryan Matlock <ryan.matlock@gmail.com>
2023-03-29 20:27:20 -0700

    :test_tube: write tests for repeat-str

--- run-length-encoding/rle-addiional-tests.el
+++ run-length-encoding/rle-addiional-tests.el
@@ -1,5 +1,6 @@
 (load-file "run-length-encoding.el")
 (declare-function str-digit-p "run-length-encoding.el" (str))
+(declare-function repeat-str "run-length-encoding.el" (n str &optional join))
 
 (ert-deftest str-digit-p-letters ()
   (let* ((uppercase (mapcar #'string (number-sequence ?A ?Z)))
@@ -15,4 +16,14 @@
   (let ((punctuation-list '("!" "@" "#" "&" ")" "<")))
     (should (equal nil (seq-every-p #'str-digit-p punctuation-list)))))
 
+(ert-deftest repeat-str-less-than-zero-error ()
+  (should-error (repeat-str -1 "foo")))
 
+(ert-deftest repeat-str-zero-empty-string ()
+  (should (equal "" (repeat-str 0 "foo"))))
+
+(ert-deftest repeat-str-expected-behavior ()
+  (should (equal "foofoofoo" (repeat-str 3 "foo"))))
+
+(ert-deftest repeat-str-dot-join ()
+  (should (equal "foo.foo.foo" (repeat-str 3 "foo" "."))))
