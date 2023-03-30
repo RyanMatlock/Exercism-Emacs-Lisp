941815ede98e599c2299d4c3915a3f153020813e
Ryan Matlock <ryan.matlock@gmail.com>
2023-03-30 14:48:06 -0700

    :test_tube: write tests for remove-all-spaces

--- luhn/luhn-additional-tests.el
+++ luhn/luhn-additional-tests.el
@@ -1,5 +1,6 @@
 (load-file "luhn.el")
 (declare-function valid-luhn-char-p "luhn.el" (c))
+(declare-function remove-all-spaces "luhn.el" (str))
 
 (ert-deftest valid-luhn-char-p-on-str ()
   (should-error (valid-luhn-char-p " ")))
@@ -12,3 +13,15 @@
 
 (ert-deftest valid-luhn-char-p-on-letter ()
   (should-not (valid-luhn-char-p ?g)))
+
+(ert-deftest remove-all-spaces-empty-string ()
+  (should (string= "" (remove-all-spaces ""))))
+
+(ert-deftest remove-all-spaces-only-spaces ()
+  (should (string= "" (remove-all-spaces "    "))))
+
+(ert-deftest remove-all-spaces-around-every-character ()
+  (should (string= "foo" (remove-all-spaces " f o o "))))
+
+(ert-deftest remove-all-spaces-scattered-spaces ()
+  (should (string= "foobar" (remove-all-spaces "fo oba   r   "))))
