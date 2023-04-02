7ef874aa42cb1eb68aac209ee6756b511c5a487e
Ryan Matlock <ryan.matlock@gmail.com>
2023-04-02 13:50:40 -0700

    :test_tube: write tests for zip-lists-alist

--- luhn/luhn-additional-tests.el
+++ luhn/luhn-additional-tests.el
@@ -1,6 +1,7 @@
 (load-file "luhn.el")
 (declare-function valid-luhn-char-p "luhn.el" (c))
 (declare-function remove-all-spaces "luhn.el" (str))
+(declare-function zip-lists-alist "luhn.el" (xs ys))
 
 (ert-deftest valid-luhn-char-p-on-str ()
   (should-error (valid-luhn-char-p " ")))
@@ -25,3 +26,21 @@
 
 (ert-deftest remove-all-spaces-scattered-spaces ()
   (should (string= "foobar" (remove-all-spaces "fo oba   r   "))))
+
+(ert-deftest zip-lists-to-alist-list-with-nil-t ()
+  (should (equal '((:foo . nil) (:bar . t) (:baz . nil) (:qux .t))
+                 (zip-lists-alist '(:foo :bar :baz :qux) '(nil t)))))
+
+(ert-deftest zip-lists-to-alist-with-nil ()
+  (should (equal '((:foo) (:bar) (:baz))
+                 (zip-lists-alist '(:foo :bar :baz) '(nil)))))
+
+(ert-deftest zip-lists-alist-longer-ys ()
+  (should (equal '((:foo . 1) (:bar . 2) (:baz . 3))
+                 (zip-lists-alist '(:foo :bar :baz) '(1 2 3 4 5)))))
+
+(ert-deftest zip-lists-alist-xs-not-sequence-error ()
+  (should-error (zip-lists-alist :foo '(1 2))))
+
+(ert-deftest zip-lists-alist-ys-not-sequence-error ()
+  (should-error (zip-lists-alist '(:foo :bar) :baz)))
