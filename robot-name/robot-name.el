c60e00d4b3e3690ec9cc68819535e04033a5bd11
Ryan Matlock <ryan.matlock@gmail.com>
2023-04-04 13:32:47 -0700

    ✅ pass Exercism test suite

--- robot-name/robot-name.el
+++ robot-name/robot-name.el
@@ -8,30 +8,35 @@
 
 (defun random-element (xs)
   "Return a random element from list XS; error if XS is not a list."
-  (cond ((sequencep xs) (nth (random (length xs)) (mapcar #'identity xs)))
+  (cond ((sequencep xs)
+         (progn
+           (random t)
+           (nth (random (length xs)) (mapcar #'identity xs))))
         (t (error "XS must be a sequence."))))
+
+(defun generate-robot-name ()
+  "Generate a random robot name of the form [A-Z]\\{2\\}[0-9]\\{3\\}."
+  (let* ((letters (number-sequence ?A ?Z))
+         (digits (number-sequence ?0 ?9))
+         (letter-prefix
+          ;; note: not using the values of the list mapcar is applied to, so
+          ;; using the dash like the underscore as a variable name in Python
+          (mapcar #'(lambda (-) (random-element letters)) '(1 2)))
+         (numbers
+          (mapcar #'(lambda (-) (random-element digits) ) '(1 2 3))))
+    (concat letter-prefix numbers)))
 
 (defun build-robot ()
   "Build a new robot with a random name."
-  (let* ((letters (number-sequence ?A ?Z))
-         (letter-limit (1+ (length letters)))
-         (digits (number-sequence ?0 ?9))
-         (digit-limit (1+ (length digits))))
-    (random t)
-    (concat
-     ;; note: not using the values of the list mapcar is applied to
-     (mapcar #'(lambda (-) (nth (random letter-limit) letters))
-             '(t t))
-     (mapcar #'(lambda (-) (nth (random digit-limit) digits))
-             '(t t t)))))
+  (list (cons :name (generate-robot-name))))
 
 (defun robot-name (robot)
   "Get the ROBOT's name."
-  (print robot))
+  (cdr (assq :name robot)))
 
 (defun reset-robot (robot)
   "Reset the name of ROBOT.  Factory reset!"
-  (setq robot (build-robot)))
+  (setcdr (assq :name robot) (generate-robot-name)))
 
 (provide 'robot-name)
 ;;; robot-name.el ends here
