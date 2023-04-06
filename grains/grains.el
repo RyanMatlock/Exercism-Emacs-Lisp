d69dad0130e8c8272755a9cd8b8c8dfcb7fca8e3
Ryan Matlock <ryan.matlock@gmail.com>
2023-04-05 23:47:57 -0700

    :tada: start exercise grains

---
+++ grains/grains.el
@@ -0,0 +1,14 @@
+;;; grains.el --- Grains exercise (exercism)  -*- lexical-binding: t; -*-
+
+;;; Commentary:
+
+(defun square (n)
+;;; Code:
+)
+
+(defun total ()
+;;; Code:
+)
+
+(provide 'grains)
+;;; grains.el ends here
