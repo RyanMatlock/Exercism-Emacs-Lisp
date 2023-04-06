ee6c7c3c3bbcead281f609f955b4c2ec6146ae50
Ryan Matlock <ryan.matlock@gmail.com>
2023-04-05 23:53:30 -0700

    ✅ pass all square and total tests

--- grains/grains.el
+++ grains/grains.el
@@ -3,11 +3,14 @@
 ;;; Commentary:
 
 (defun square (n)
-;;; Code:
-)
+  "Return the number of grains on square N of a chessboard, where square 1 has
+1 grain, and each successive square is double the previous grain."
+  (expt 2 (1- n)))
 
 (defun total ()
-;;; Code:
+  "Return the total number of grains on the chessboard."
+  (let ((board (number-sequence 1 64)))
+    (apply #'+ (mapcar #'square board)))
 )
 
 (provide 'grains)
