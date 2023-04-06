6d5bfe34afa8ce938677440980bc8a2633bfaa57
Ryan Matlock <ryan.matlock@gmail.com>
2023-04-06 14:44:47 -0700

    :tada: start exercise armstrong-numbers

---
+++ armstrong-numbers/armstrong-numbers.el
@@ -0,0 +1,10 @@
+;;; armstrong-numbers.el --- armstrong-numbers Exercise (exercism)  -*- lexical-binding: t; -*-
+
+;;; Commentary:
+
+(defun armstrong-p (n)
+;;; Code:
+)
+
+(provide 'armstrong-numbers)
+;;; armstrong-numbers.el ends here
