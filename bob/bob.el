d2848f66f37cbf835dca8d9e71c8e43624b97e9f
Ryan Matlock <ryan.matlock@gmail.com>
2023-04-07 16:32:57 -0700

    :recycle: use regexps instead of character checking

--- bob/bob.el
+++ bob/bob.el
@@ -2,45 +2,46 @@
 
 ;;; Commentary:
 
-(defun capitalp (c)
-  "Return T if char C is a capital letter (in range A-Z); otherwise, return
-NIL; error if C is not a char."
-  (cond ((and (numberp c) (char-or-string-p c))
-         (and (>= c ?A) (<= c ?Z)))
-        (t (error "C must be a char."))))
+;; (defun capitalp (c)
+;;   "Return T if char C is a capital letter (in range A-Z); otherwise, return
+;; NIL; error if C is not a char."
+;;   (cond ((and (numberp c) (char-or-string-p c))
+;;          (and (>= c ?A) (<= c ?Z)))
+;;         (t (error "C must be a char."))))
 
-(defun lowercasep (c)
-  "Return T if char C is a lowercase letter (in range a-z); otherwise, return
-NIL; error if C is not a char."
-  (cond ((and (numberp c) (char-or-string-p c))
-         (and (>= c ?a) (<= c ?z)))
-        (t (error "C must be a char."))))
+;; (defun lowercasep (c)
+;;   "Return T if char C is a lowercase letter (in range a-z); otherwise, return
+;; NIL; error if C is not a char."
+;;   (cond ((and (numberp c) (char-or-string-p c))
+;;          (and (>= c ?a) (<= c ?z)))
+;;         (t (error "C must be a char."))))
 
-(defun whitespacep (c)
-  (let ((whitespace-chars (mapcar #'string-to-char '(" " "\t" "\n" "\r"))))
-    (cond ((and (numberp c) (char-or-string-p c))
-           (seq-some #'(lambda (whitespace-char) (= whitespace-char c))
-                     whitespace-chars)))))
+;; (defun whitespacep (c)
+;;   (let ((whitespace-chars (mapcar #'string-to-char '(" " "\t" "\n" "\r"))))
+;;     (cond ((and (numberp c) (char-or-string-p c))
+;;            (seq-some #'(lambda (whitespace-char) (= whitespace-char c))
+;;                      whitespace-chars)))))
+
+(defun re-string-match-p (regexp str &optional start)
+  "Works like STRING-MATCH-P except instead of returning an index when REGEXP
+matches STR, it returns T; otherwise, it returns NIL."
+  (let ((case-fold-search nil))
+    (not (null (string-match-p regexp str start)))))
 
 (defun questionp (sentence)
   "Return T if string SENTENCE ends in '?'; otherwise return NIL; error if
 SENTENCE is not a string."
-  (cond ((stringp sentence)
-         (let* ((sentence-no-ws (seq-filter
-                                 #'(lambda (c) (not (whitespacep c)))
-                                 sentence))
-                (last-non-whitespace
-                 (string (elt sentence-no-ws
-                              (1- (length sentence-no-ws))))))
-           (string= last-non-whitespace "?")))
+  (cond ((stringp sentence) (re-string-match-p "[?][[:space:]]*$" sentence))
         (t (error "SENTENCE must be a string."))))
 
 (defun all-letters-capital-p (sentence)
   "Return T if string SENTENCE contains only capital letters and punctuation;
 otherwise, return NIL; error if SENTENCE is not a string."
-  (cond ((stringp sentence)
-         (and (seq-some #'capitalp sentence)
-              (not (seq-some #'lowercasep sentence))))
+  (cond ((stringp sentence) (and
+                             ;; *some* capital letters
+                             (re-string-match-p "[A-Z]+" sentence)
+                             ;; AND *no* lowercase letters
+                             (not (re-string-match-p "[a-z]+" sentence))))
         (t (error "SENTENCE must be a string."))))
 
 (defun response-for (phrase)
@@ -51,13 +52,15 @@ If PHRASE is an all caps question, respond with, 'Calm down, I know what I'm
 doing!'
 If PHRASE is the empty string, respond with, 'Fine. Be that way!'
 If PHRASE is anything else, respond with, 'Whatever.'"
-  (cond ((or (seq-empty-p phrase) (seq-every-p #'whitespacep phrase))
-         "Fine. Be that way!")
-        ((and (all-letters-capital-p phrase) (questionp phrase))
-         "Calm down, I know what I'm doing!")
-        ((all-letters-capital-p phrase) "Whoa, chill out!")
-        ((questionp phrase) "Sure.")
-        (t "Whatever.")))
+  (let ((phrase-sans-newlines (string-replace "\n" " " phrase)))
+    (cond ((re-string-match-p "^[[:space:]]*$" phrase-sans-newlines)
+           "Fine. Be that way!")
+          ((and (all-letters-capital-p phrase-sans-newlines)
+                (questionp phrase-sans-newlines))
+           "Calm down, I know what I'm doing!")
+          ((all-letters-capital-p phrase-sans-newlines) "Whoa, chill out!")
+          ((questionp phrase-sans-newlines) "Sure.")
+          (t "Whatever."))))
 
 (provide 'bob)
 ;;; bob.el ends here
