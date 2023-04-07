62ea68f7601690e560133165e92eaa8cbcd38a87
Ryan Matlock <ryan.matlock@gmail.com>
2023-04-06 23:07:59 -0700

    ✅ pass all helper function tests

--- bob/bob-additional-tests.el
+++ bob/bob-additional-tests.el
@@ -1,8 +1,9 @@
 (load-file "bob.el")
 
 (declare-function capitalp "bob.el" (c))
+(declare-function lowercasep "bob.el" (c))
 (declare-function questionp "bob.el" (sentence))
-(declare-function empty-string-p "bob.el" (str))
+(declare-function all-letters-capital-p "bob.el" (sentence))
 
 (ert-deftest capitalp-all-capital-letters ()
   (should (seq-every-p #'capitalp (number-sequence ?A ?Z))))
@@ -16,6 +17,18 @@
 (ert-deftest capitalp-error-on-string ()
   (should-error (capitalp "A")))
 
+(ert-deftest lowercasep-all-lowercase-letters ()
+  (should (seq-every-p #'lowercasep (number-sequence ?a ?z))))
+
+(ert-deftest lowercasep-fail-on-uppercase-char ()
+  (should-not (lowercasep ?A)))
+
+(ert-deftest lowercasep-fail-on-punctuation-char ()
+  (should-not (lowercasep (string-to-char "!"))))
+
+(ert-deftest lowercasep-error-on-string ()
+  (should-error (lowercasep "a")))
+
 (ert-deftest questionp-not-a-question ()
   (should-not (questionp "This is not a question.")))
 
@@ -25,11 +38,11 @@
 (ert-deftest questionp-not-a-string ()
   (should-error (questionp '("f" "o" "o" "?"))))
 
-(ert-deftest empty-string-p-empty-string ()
-  (should (empty-string-p "")))
+(ert-deftest all-letters-capital-p-you-maniacs ()
+  (should (all-letters-capital-p "YOU MANIACS! YOU BLEW IT UP!")))
 
-(ert-deftest empty-string-p-non-empty-string ()
-  (should-not (empty-string-p ".")))
+(ert-deftest all-letters-capital-p-calm ()
+  (should-not (all-letters-capital-p "Oh hai")))
 
-(ert-deftest empty-string-p-non-string ()
-  (should-error (empty-string-p '())))
+(ert-deftest all-letters-capital-p-list ()
+  (should-error (all-letters-capital-p '(?F ?O ?O ?B ?A ?R))))
--- bob/bob.el
+++ bob/bob.el
@@ -2,9 +2,39 @@
 
 ;;; Commentary:
 
+(defun capitalp (c)
+  "Return T if char C is a capital letter (in range A-Z); otherwise, return
+NIL; error if C is not a char."
+  (cond ((and (numberp c) (char-or-string-p c))
+         (and (>= c ?A) (<= c ?Z)))
+        (t (error "C must be a char."))))
+
+(defun lowercasep (c)
+  "Return T if char C is a lowercase letter (in range a-z); otherwise, return
+NIL; error if C is not a char."
+  (cond ((and (numberp c) (char-or-string-p c))
+         (and (>= c ?a) (<= c ?z)))
+        (t (error "C must be a char."))))
+
+(defun questionp (sentence)
+  "Return T if string SENTENCE ends in '?'; otherwise return NIL; error if
+SENTENCE is not a string."
+  (cond ((stringp sentence)
+         (let ((last (string (elt sentence (1- (length sentence))))))
+           (string= last "?")))
+        (t (error "SENTENCE must be a string."))))
+
+(defun all-letters-capital-p (sentence)
+  "Return T if string SENTENCE contains only capital letters and punctuation;
+otherwise, return NIL; error if SENTENCE is not a string."
+  (cond ((stringp sentence)
+         (and (seq-some #'capitalp sentence)
+              (not (seq-some #'lowercasep sentence))))
+        (t (error "SENTENCE must be a string."))))
+
 (defun response-for (phrase)
 ;;; Code:
-)  
+  )
 
 (provide 'bob)
 ;;; bob.el ends here
