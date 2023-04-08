3e667fa5b08a806ff347857b45b398cfbaabb635
Ryan Matlock <ryan.matlock@gmail.com>
2023-04-07 19:31:27 -0700

    :tada: start exercise perfect numbers

---
+++ perfect-numbers/perfect-numbers.el
@@ -0,0 +1,10 @@
+;;; perfect-numbers.el --- perfect-numbers Exercise (exercism)  -*- lexical-binding: t; -*-
+
+;;; Commentary:
+
+(defun classify (number)
+;;; Code:
+)
+
+(provide 'perfect-numbers)
+;;; perfect-numbers.el ends here
