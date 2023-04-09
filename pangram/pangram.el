bc4145d5f3977b0fabfaa7bfcf4dca5e0e09f716
Ryan Matlock <ryan.matlock@gmail.com>
2023-04-09 12:39:46 -0700

    ✅ pass all pangramp tests

--- pangram/pangram.el
+++ pangram/pangram.el
@@ -3,8 +3,17 @@
 ;;; Commentary:
 
 (defun pangramp (phrase)
-;;; Code:
-)
+  "Return T if phrase contains every letter of the alphabet; otherwise, return
+NIL."
+  (let* ((alphabet (number-sequence ?a ?z))
+         (alpha-alist (mapcar #'(lambda (c) (cons c nil)) alphabet)))
+    (seq-mapn #'(lambda (c)
+                  (let ((entry (assq c alpha-alist)))
+                    (if entry
+                        (setcdr entry t)
+                      nil)))
+              (downcase phrase))
+    (seq-every-p #'(lambda (aa-elem) (cdr aa-elem)) alpha-alist)))
 
 (provide 'pangram)
 ;;; pangram.el ends here
