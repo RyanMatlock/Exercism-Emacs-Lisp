0f57c464b686e04cfddfe7bbff0c02ad5f4b5330
Ryan Matlock <ryan.matlock@gmail.com>
2023-04-08 19:37:10 -0700

    :tada: start exercise phone-number

---
+++ phone-number/phone-number-test.el
@@ -0,0 +1,55 @@
+;;; phone-number-test.el --- Tests for phone-number (exercism)  -*- lexical-binding: t; -*-
+
+;;; Commentary:
+
+;;; Code:
+
+(load-file "phone-number.el")
+(declare-function numbers "phone-number.el" (num))
+(declare-function area-code "phone-number.el" (num))
+(declare-function pprint "phone-number.el" (num))
+
+(ert-deftest cleans-number-test ()
+  (should (equal (numbers "(123) 456-7890") "1234567890")))
+
+
+(ert-deftest cleans-numbers-with-dots-test ()
+  (should (equal (numbers "123.456.7890") "1234567890")))
+
+
+(ert-deftest valid-when-11-digits-and-first-is-1-test ()
+  (should (equal (numbers "11234567890") "1234567890")))
+
+
+(ert-deftest invalid-when-11-digits-test ()
+  (should (equal (numbers "21234567890") "0000000000")))
+
+
+(ert-deftest invalid-when-9-digits-test ()
+  (should (equal (numbers "123456789") "0000000000")))
+
+(ert-deftest invalid-when-more-than-11-digits-test ()
+  (should (equal (numbers "321234567890") "0000000000")))
+
+(ert-deftest invalid-with-letters ()
+  (should (equal (numbers "523-abc-7890") "0000000000")))
+
+
+(ert-deftest invalid-with-punctuations ()
+  (should (equal (numbers "523-@:!-7890") "0000000000")))
+
+
+(ert-deftest area-code-test ()
+  (should (equal (area-code "1234567890") "123")))
+
+
+(ert-deftest pprint-test ()
+  (should (equal (pprint "1234567890") "(123) 456-7890")))
+
+
+(ert-deftest pprint-full-us-phone-number-test ()
+  (should (equal (pprint "11234567890") "(123) 456-7890")))
+
+
+(provide 'phone-number)
+;;; phone-number-test.el ends here
---
+++ phone-number/phone-number.el
@@ -0,0 +1,18 @@
+;;; phone-number.el --- phone-number Exercise (exercism)  -*- lexical-binding: t; -*-
+
+;;; Commentary:
+
+(defun numbers (num)
+;;; Code:
+)
+
+(defun area-code (num)
+;;; Code:
+)
+
+(defun pprint (num)
+;;; Code:
+)
+
+(provide 'phone-number)
+;;; phone-number.el ends here
