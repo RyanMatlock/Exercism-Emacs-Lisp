6a372d5eb12b3218039c4f8f53cfc471ddcfe550
Ryan Matlock <ryan.matlock@gmail.com>
2023-04-09 12:18:01 -0700

    :rocket: finish the exercise on first attempt

--- difference-of-squares/difference-of-squares.el
+++ difference-of-squares/difference-of-squares.el
@@ -1,18 +1,25 @@
 ;;; difference-of-squares.el --- Difference of Squares (exercism)  -*- lexical-binding: t; -*-
 
-;;; Commentary:
+;;; Commentary: got it on the first try. Based on the prompt, I thought there'd
+;;; be an issue where the naive solution was impractically inefficient
+;;; ¯\_(ツ)_/¯
 
 (defun sum-of-squares (n)
-;;; Code:
-)
+  "Return the sum of the square of numbers 1 through N."
+  (let* ((1-to-n (number-sequence 1 n))
+         (squares (mapcar #'(lambda (x) (* x x)) 1-to-n)))
+    (apply #'+ squares)))
 
 (defun square-of-sum (n)
-;;; Code:
-)
+  "Return the square of the sum of the numbers 1 through N."
+  (let* ((1-to-n (number-sequence 1 n))
+         (sum (apply #'+ 1-to-n)))
+    (* sum sum)))
 
 (defun difference (n)
-;;; Code:
-)
+  "Return the difference of square of the sum of numbers 1 through N and the
+sum of each number 1 through N squared."
+  (- (square-of-sum n) (sum-of-squares n)))
 
 (provide 'difference-of-squares)
 ;;; difference-of-squares.el ends here
