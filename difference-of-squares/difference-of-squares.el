8b2e0f54a6a915fd4b2ec158ff1f83e0186aaeb7
Ryan Matlock <ryan.matlock@gmail.com>
2023-04-09 12:10:02 -0700

    :tada: start exercise difference-of-squares

---
+++ difference-of-squares/difference-of-squares.el
@@ -0,0 +1,18 @@
+;;; difference-of-squares.el --- Difference of Squares (exercism)  -*- lexical-binding: t; -*-
+
+;;; Commentary:
+
+(defun sum-of-squares (n)
+;;; Code:
+)
+
+(defun square-of-sum (n)
+;;; Code:
+)
+
+(defun difference (n)
+;;; Code:
+)
+
+(provide 'difference-of-squares)
+;;; difference-of-squares.el ends here
