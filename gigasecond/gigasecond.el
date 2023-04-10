8729b45aed825d3a2213bc9d0005c240179c8200
Ryan Matlock <ryan.matlock@gmail.com>
2023-04-10 08:47:34 -0700

    :construction: attempt to figure out alternate solutions
    My previous solutions pass locally, but Exercism rejects some of the functions
    they use.

--- gigasecond/gigasecond.el
+++ gigasecond/gigasecond.el
@@ -10,17 +10,22 @@
 (defun from (second minute hour day month year)
   "Add 10^9 seconds to the timestamp specified by SECOND, MINUTE, HOUR, DAY,
 MONTH, and YEAR."
-  (let* ((args (list second minute hour day month year))
-         (t-init (make-decoded-time
-                  :second second
-                  :minute minute
-                  :hour hour
-                  :day day
-                  :month month
-                  :year year))
-         (giga (expt 10 9))
-         (gigasecond (make-decoded-time :second giga)))
-    (take (length args) (decoded-time-add t-init gigasecond))))
+  ;; (defun --take (n seq)
+  ;;   "Exercism says take is a void function for some reason"
+  ;;   (let* ((ts (make-list n t))
+  ;;          (nils (make-list (- (length seq) n) nil))
+  ;;          (ts-nils (append ts nils))
+  ;;          (seq-alist (seq-mapn #'(lambda (x bool) (cons x bool))
+  ;;                               seq
+  ;;                               ts-nils)))
+  ;;     (mapcar #'car (seq-filter
+  ;;                    #'(lambda (alist-elem) (cdr alist-elem)) seq-alist))))
+  ;; (let* ((args (list second minute hour day month year))
+  ;;        (t-init (list second minute hour day month year))
+  ;;        (giga (expt 10 9)))
+  ;;   (--take (length args) (decoded-time-add t-init (list giga))))
+  ()
+  )
 
 (provide 'gigasecond)
 ;;; gigasecond.el ends here
