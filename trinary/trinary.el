70806f103308c38135648b6932b06a4aeb7aa26e
Ryan Matlock <ryan.matlock@gmail.com>
2023-04-10 09:18:51 -0700

    :recycle: validate input with regexp

--- trinary/trinary.el
+++ trinary/trinary.el
@@ -5,19 +5,16 @@
 (defun trinary-to-decimal (string)
   "Convert STRING representing a trinary number to its decimal equivalent; if
 STRING contains invalid characters (i.e. other than 0, 1, or 2), return 0."
-  (cond ((= (length string)
-            (length (seq-filter #'(lambda (c) (or (= c ?0)
-                                                  (= c ?1)
-                                                  (= c ?2)))
-                                string)))
-         (let ((base 3)
-               (power (1- (length string))))
-           (apply #'+ (seq-mapn #'(lambda (n index)
-                                    (* (string-to-number (string n))
-                                       (expt 3 (- power index))))
-                                string
-                                (number-sequence 0 power)))))
-        (t 0)))
+  (let ((valid-trinary-regexp "^[012]+$"))
+    (cond ((string-match-p valid-trinary-regexp string)
+           (let ((base 3)
+                 (power (1- (length string))))
+             (apply #'+ (seq-mapn #'(lambda (n index)
+                                      (* (string-to-number (string n))
+                                         (expt 3 (- power index))))
+                                  string
+                                  (number-sequence 0 power)))))
+          (t 0))))
 
 (provide 'trinary)
 ;;; trinary.el ends here
