8010d5fe0ebd8a00ca4ae5e63995f9ec593c8692
Ryan Matlock <ryan.matlock@gmail.com>
2023-04-10 00:14:03 -0700

    :tada: start exercise gigasecond

---
+++ gigasecond/gigasecond.el
@@ -0,0 +1,15 @@
+;;; gigasecond.el --- Gigasecond exercise (exercism)  -*- lexical-binding: t; -*-
+
+;;; Commentary:
+;; Calculate the date one gigasecond (10^9 seconds) from the
+;; given date.
+;;
+;; NB: Pay attention to  Emacs' handling of time zones and dst
+;; in the encode-time and decode-time functions.
+
+(defun from (second minute hour day month year)
+;;; Code:
+)
+
+(provide 'gigasecond)
+;;; gigasecond.el ends here
