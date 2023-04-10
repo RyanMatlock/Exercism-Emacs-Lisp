8d3cf3b565db2396f11c1bd9b4ec3a2bb7c3af26
Ryan Matlock <ryan.matlock@gmail.com>
2023-04-10 11:21:24 -0700

    ✅ pass all encode tests

--- atbash-cipher/atbash-cipher.el
+++ atbash-cipher/atbash-cipher.el
@@ -15,8 +15,22 @@
 
 (defun encode (plaintext)
   "Encode PLAINTEXT to atbash-cipher encoding."
-  ;;; Code:
-)
+  (let* ((alphabet (number-sequence ?a ?z))
+         (atbash-alist (seq-mapn #'(lambda (k v) (cons k v))
+                                 alphabet (reverse alphabet)))
+         (atbash-valid-char-regexp "[[:alnum:]]"))
+    (chunk-text 5
+                (mapconcat #'string
+                           (seq-filter
+                            #'(lambda (c)
+                                (string-match-p atbash-valid-char-regexp
+                                                (string c)))
+                            ;; replace key with value
+                            (seq-mapn
+                             #'(lambda (c)
+                                 (or (alist-get c atbash-alist) c))
+                             (downcase plaintext)))
+                           ""))))
 
 (provide 'atbash-cipher)
 ;;; atbash-cipher.el ends here
