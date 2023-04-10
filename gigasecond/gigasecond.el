5d3edce5266da65744e50e266ab9a21f574881fe
Ryan Matlock <ryan.matlock@gmail.com>
2023-04-10 00:44:23 -0700

    ✅ pass from tests

--- gigasecond/gigasecond.el
+++ gigasecond/gigasecond.el
@@ -8,8 +8,19 @@
 ;; in the encode-time and decode-time functions.
 
 (defun from (second minute hour day month year)
-;;; Code:
-)
+  "Add 10^9 seconds to the timestamp specified by SECOND, MINUTE, HOUR, DAY,
+MONTH, and YEAR."
+  (let* ((args (list second minute hour day month year))
+         (t-init (make-decoded-time
+                  :second second
+                  :minute minute
+                  :hour hour
+                  :day day
+                  :month month
+                  :year year))
+         (giga (expt 10 9))
+         (gigasecond (make-decoded-time :second giga)))
+    (take (length args) (decoded-time-add t-init gigasecond))))
 
 (provide 'gigasecond)
 ;;; gigasecond.el ends here
