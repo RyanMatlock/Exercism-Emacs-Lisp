7966d55354217d108f2f0e4b7ec1dfe74661458d
Ryan Matlock <ryan.matlock@gmail.com>
2023-04-10 09:31:42 -0700

    :tada: start exercise atbash-cipher

---
+++ atbash-cipher/atbash-cipher.el
@@ -0,0 +1,11 @@
+;;; atbash-cipher.el --- Atbash-Cipher (exercism)  -*- lexical-binding: t; -*-
+
+;;; Commentary:
+
+(defun encode (plaintext)
+  "Encode PLAINTEXT to atbash-cipher encoding."
+  ;;; Code:
+)
+
+(provide 'atbash-cipher)
+;;; atbash-cipher.el ends here
