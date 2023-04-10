58ce86879ed854c77c5a6d87fc57777641e32ad2
Ryan Matlock <ryan.matlock@gmail.com>
2023-04-10 09:06:08 -0700

    ✅ pass all tests

--- trinary/trinary.el
+++ trinary/trinary.el
@@ -5,13 +5,19 @@
 (defun trinary-to-decimal (string)
   "Convert STRING representing a trinary number to its decimal equivalent; if
 STRING contains invalid characters (i.e. other than 0, 1, or 2), return 0."
-  (let ((base 3)
-        (power (1- (length string))))
-    (apply #'+ (seq-mapn #'(lambda (n index)
-                             (* (string-to-number (string n))
-                                (expt 3 (- power index))))
-                         string
-                         (number-sequence 0 power)))))
+  (cond ((= (length string)
+            (length (seq-filter #'(lambda (c) (or (= c ?0)
+                                                  (= c ?1)
+                                                  (= c ?2)))
+                                string)))
+         (let ((base 3)
+               (power (1- (length string))))
+           (apply #'+ (seq-mapn #'(lambda (n index)
+                                    (* (string-to-number (string n))
+                                       (expt 3 (- power index))))
+                                string
+                                (number-sequence 0 power)))))
+        (t 0)))
 
 (provide 'trinary)
 ;;; trinary.el ends here
