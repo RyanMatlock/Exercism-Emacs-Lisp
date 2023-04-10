17a985c2f81a7f97bdb3a5bbcfc0e623e9ceaf92
Ryan Matlock <ryan.matlock@gmail.com>
2023-04-10 08:49:00 -0700

    :tada: start project trinary

---
+++ trinary/trinary.el
@@ -0,0 +1,10 @@
+;;; trinary.el --- Trinary (exercism)  -*- lexical-binding: t; -*-
+
+;;; Commentary:
+
+(defun trinary-to-decimal (string)
+;;; Code:
+)
+
+(provide 'trinary)
+;;; trinary.el ends here
