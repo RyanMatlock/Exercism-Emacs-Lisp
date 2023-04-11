bbb22d721fab925193c25f16ed65828e732a3a6c
Ryan Matlock <ryan.matlock@gmail.com>
2023-04-10 22:21:48 -0700

    :recycle: update conversion of chars to numbers

--- armstrong-numbers/armstrong-numbers-additional-tests.el
+++ armstrong-numbers/armstrong-numbers-additional-tests.el
@@ -1,5 +1,18 @@
 (load-file "armstrong-numbers.el")
+(declare-function char-to-number "armstrong-numbers.el" (c))
 (declare-function wholenum-to-digits "armstrong-numbers.el" (num))
+
+(ert-deftest char-to-number-zero ()
+  (should (= 0 (char-to-number ?0))))
+
+(ert-deftest char-to-number-nine ()
+  (should (= 9 (char-to-number ?9))))
+
+(ert-deftest char-to-number-error-on-letter ()
+  (should-error (char-to-number ?A)))
+
+(ert-deftest char-to-number-should-error-on-string-digit ()
+  (should-error (char-to-number "4")))
 
 (ert-deftest wholenum-to-digits-zero ()
   (should (equal '(0) (wholenum-to-digits 0))))
--- armstrong-numbers/armstrong-numbers.el
+++ armstrong-numbers/armstrong-numbers.el
@@ -2,11 +2,22 @@
 
 ;;; Commentary:
 
+(defun char-to-number (c)
+  "Convert char C to number between 0 and 9; error if C is not in that range."
+  (cond ((characterp c)
+         (let ((maybe-digit
+                ;; saw this in a community solution to another exercise
+                (- c ?0)))
+           (if (and (>= maybe-digit 0) (<= maybe-digit 9))
+               maybe-digit
+             (error "Char C out of range."))))
+        (t (error "Input must be of type char."))))
+
 (defun wholenum-to-digits (num)
   "Turn NUM into a list of digits; error if NUM is not a whole number."
   (cond
    ((wholenump num)
-    (seq-mapn #'(lambda (c) (string-to-number (string c))) (format "%d" num)))
+    (seq-mapn #'char-to-number (format "%d" num)))
    (t (error "NUM must be a whole number."))))
 
 (defun armstrong-p (n)
