92026a77a6a8d260ed693aa32d98ad06bf06a574
Ryan Matlock <ryan.matlock@gmail.com>
2023-04-10 23:49:14 -0700

    :tada: start exercise crypto-square

---
+++ crypto-square/crypto-square.el
@@ -0,0 +1,10 @@
+;;; crypto-square.el --- Crypto Square (exercism)  -*- lexical-binding: t; -*-
+
+;;; Commentary:
+
+(defun encipher (plaintext)
+;;; Code:
+)
+
+(provide 'crypto-square)
+;;; crypto-square.el ends here
