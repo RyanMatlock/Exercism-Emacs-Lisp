7dc0918e21c622f5b23e8534c82ab7a2086a34e7
Ryan Matlock <ryan.matlock@gmail.com>
2023-04-11 01:00:38 -0700

    ✅ write helper functions normalize and rectangle-values

---
+++ crypto-square/crypto-square-additional-test.el
@@ -0,0 +1,22 @@
+(load-file "crypto-square.el")
+(declare-function normalize "crypto-square.el" (text))
+(declare-function rectangle-values "crypto-square.el" (seq))
+
+(ert-deftest normalize-already-normal ()
+  (let ((normal-text "foobarbaz"))
+    (should (string= normal-text (normalize normal-text)))))
+
+(ert-deftest normalize-non-string-input-error ()
+  (should-error (normalize (mapcar #'string "foobar"))))
+
+(ert-deftest normalize-punctuation-and-capitalization ()
+  (should (string= "foobarbaz" (normalize "Foo, bar -- BAZ?!"))))
+
+(ert-deftest rectangle-values-54 ()
+  (should (equal '(8 . 7) (rectangle-values (make-list 54 t)))))
+
+(ert-deftest rectangle-values-square-length ()
+  (should (equal '(4 . 4) (rectangle-values (make-list 16 t)))))
+
+(ert-deftest rectangle-values-prime-length ()
+  (should (equal '(5 . 4) (rectangle-values (make-list 17 t)))))
--- crypto-square/crypto-square.el
+++ crypto-square/crypto-square.el
@@ -2,9 +2,35 @@
 
 ;;; Commentary:
 
+(defun normalize (text)
+  "Downcase and remove spaces and punctuation from string TEXT."
+  (cond ((stringp text)
+         (mapconcat #'string
+                    (seq-filter
+                     #'(lambda (c) (string-match-p "[a-z0-9]" (string c)))
+                     (downcase text))
+                    ""))
+        (t (error "TEXT must be a string."))))
+
+(defun rectangle-values (seq)
+  "For sequence SEQ, find the smallest integer C such that:
+    R * C >= (LENGTH SEQ),
+    C >= R, AND
+    C - R <= 1
+and return them as (CONS C R).
+
+Note that C is for columns and R is for rows."
+  (cond ((sequencep seq)
+         (let* ((len (length seq))
+                (c (ceiling (sqrt len))))
+           (if (= (* c c) len)
+               (cons c c)
+             (cons c (1- c)))))
+        (t (error "SEQ must be a sequence."))))
+
 (defun encipher (plaintext)
 ;;; Code:
-)
+  )
 
 (provide 'crypto-square)
 ;;; crypto-square.el ends here
