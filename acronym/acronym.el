335d73c615e4bc425fe121388226ce50e2547c15
Ryan Matlock <ryan.matlock@gmail.com>
2023-04-17 18:30:49 -0700

    :recycle: simplify implementation of acronym using let* expression

--- acronym/acronym.el
+++ acronym/acronym.el
@@ -1,27 +1,14 @@
 ;;; acronym.el --- Acronym (exercism)  -*- lexical-binding: t; -*-
 
-;;; Commentary: This is probably an overly verbose way of doing it, but it's
-;;; also easier (for me) to understand this way.
+;;; Commentary:
 
 (defun acronym (phrase)
-  "Convert phrase to acronym. Hyphens count as whitespace, and all other
+  "Convert string PHRASE to an acronym (i.e. concatenating the uppercase first
+letter of each word). Hyphens and whitespace are word separators, and all other
 punctuation is considered part of the word to which it's atttached."
-  (defun hyphens-to-whitespace (phrase)
-    "Replace hyphens with whitespace."
-    (replace-regexp-in-string "-" " " phrase))
-  (defun phrase-to-words (phrase)
-    "Split phrase on whitespace characters."
-    (split-string phrase "[[:space:]]+"))
-  (defun word-to-letters (word)
-    "Turn a word into a list of letters."
-    (mapcar #'string word))
-  (defun extract-initials-from-words (words)
-    "Extract first letter of each word from list of words"
-    (mapcar #'car (mapcar #'word-to-letters words)))
-  (upcase (mapconcat
-           #'identity
-           (extract-initials-from-words
-            (phrase-to-words (hyphens-to-whitespace phrase))) "")))
+  (let* ((words (split-string phrase "[-[:space:]]+"))
+         (initial-chars (mapcar #'(lambda (word) (elt word 0)) words)))
+    (upcase (mapconcat #'string initial-chars ""))))
 
 (provide 'acronym)
 ;;; acronym.el ends here
