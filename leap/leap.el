f20a65ff03a4f4ecf5e6790a09e97cdb141a338c
Ryan Matlock <ryan.matlock@gmail.com>
2023-04-19 05:53:44 -0700

    :recycle: write more idiomatic verison of leap-year-p
    The type checking is probably overkill, but ¯\_(ツ)_/¯

--- leap/leap.el
+++ leap/leap.el
@@ -2,21 +2,20 @@
 
 ;;; Commentary:
 
-(defun leap-year-p (year)
-  "Determine if a year is a leap year."
-  (defun divisible-by-p (number divisor)
-    "Determine if number is divisible by divisor."
-    (eq (mod number divisor) 0))
-  (cond ((divisible-by-p year 400) t)
-        ((divisible-by-p year 100) nil)
-        ((divisible-by-p year 4) t)
-        (t nil)))
+(defun divisorp (number divisor)
+  "Return T if NUMBER is evenly divisible by DIVISOR; else return nil; error if
+number and divisor aren't integers."
+  (cond ((not (integerp number)) (error "NUMBER must be an integer."))
+        ((not (integerp divisor)) (error "DIVISOR must be an integer."))
+        (t (zerop (mod number divisor)))))
 
-;; -- IELM testing --
-;; ELISP> (divisible-by-p 21 7)
-;; t
-;; ELISP> (divisible-by-p 22 7)
-;; nil
+(defun leap-year-p (year)
+  "Determine if a YEAR is a leap year."
+  (cond ((integerp year)
+         (or (divisorp year 400)
+             (and (not (divisorp year 100))
+                  (divisorp year 4))))
+        (t (error "YEAR must be an integer."))))
 
 (provide 'leap-year-p)
 ;;; leap.el ends here
