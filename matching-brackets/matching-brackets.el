eadb9e51466c732b458b6eda0859e3cabee0cf3d
Ryan Matlock <ryan.matlock@gmail.com>
2023-05-21 17:45:25 -0700

    ✅ pass all tests

--- matching-brackets/matching-brackets.el
+++ matching-brackets/matching-brackets.el
@@ -6,8 +6,21 @@
 
 
 (defun is-paired (value)
-  (error
-   "Delete this S-Expression and write your own implementation"))
+  "Return T if string VALUE contains paired brackets (i.e. (), [], and {},
+which may be nested); otherwise return NIL."
+  (let* ((only-brackets
+          (replace-regexp-in-string "[^][(){}]+" "" value))
+         (regexp-or "\\|")
+         (matched-pairs-removed
+          (replace-regexp-in-string (concat "\\(\\{\\}\\)" ;; {}
+                                            regexp-or
+                                            "\\(\\[\\]\\)" ;; []
+                                            regexp-or
+                                            "\\(()\\)+") ;; ()
+                                    "" only-brackets)))
+    (cond ((string= only-brackets matched-pairs-removed) nil)
+          ((string= "" matched-pairs-removed) t)
+          (t (is-paired matched-pairs-removed)))))
 
 
 (provide 'matching-brackets)
