f789a54eb0c3240199debbef0ed69084fa79a36a
Ryan Matlock <ryan.matlock@gmail.com>
2023-05-21 19:27:57 -0700

    ✅ pass key-is-made-only-of-lowercase-letters

--- simple-cipher/simple-cipher.el
+++ simple-cipher/simple-cipher.el
@@ -5,6 +5,8 @@
 ;;; Code:
 
 
+(defconst sc--min-random-key-length 100)
+
 (defun encode (plaintext key)
   (error
    "Delete this S-Expression and write your own implementation"))
@@ -14,8 +16,13 @@
    "Delete this S-Expression and write your own implementation"))
 
 (defun generate-key ()
-  (error
-   "Delete this S-Expression and write your own implementation"))
+  "Generate a random lowercase key of length 'sc--min-random-key-length'."
+  (let ((key (make-string sc--min-random-key-length ?a)))
+    ;; random returns an integer in the range [0, limit), so ?a + 0 = ?a and
+    ;; ?a + (1- 26) = ?z, so all letters are possible
+    (mapconcat #'string
+               (seq-map #'(lambda (c) (+ c (random sc--num-letters))) key)
+               "")))
 
 
 (provide 'simple-cipher)
