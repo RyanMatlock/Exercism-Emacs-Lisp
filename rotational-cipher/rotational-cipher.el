0f1b8cda8c97715c5f52daff84acc69c6f0c40e4
Ryan Matlock <ryan.matlock@gmail.com>
2023-05-21 18:14:19 -0700

    ✅ pass all tests

--- rotational-cipher/rotational-cipher.el
+++ rotational-cipher/rotational-cipher.el
@@ -6,8 +6,23 @@
 
 
 (defun rotate (text shift-key)
-  (error
-   "Delete this S-Expression and write your own implementation"))
+  "Take modulo 26 of the sum of number SHIFT-KEY (between 0 and 26) and each
+letter char in string TEXT and return the result as a string."
+
+  (defun shift (char shift-key first-letter)
+    (+ (mod (+ (- char first-letter) shift-key) 26)
+       first-letter))
+
+  (let ((uppercase (number-sequence ?A ?Z))
+        (lowercase (number-sequence ?a ?z)))
+    (mapconcat
+     #'string
+     (seq-map #'(lambda (c)
+                  (cond ((member c uppercase) (shift c shift-key ?A))
+                        ((member c lowercase) (shift c shift-key ?a))
+                        (t c)))
+              text)
+     "")))
 
 
 (provide 'rotational-cipher)
