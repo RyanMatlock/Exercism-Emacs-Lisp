2064962108a10df364ca477781a8315361f1bb51
Ryan Matlock <ryan.matlock@gmail.com>
2023-05-23 10:29:26 -0700

    ✅ pass valid-position-p tests

--- queen-attack/queen-attack.el
+++ queen-attack/queen-attack.el
@@ -6,8 +6,19 @@
 
 
 (defun valid-position-p (queen)
-  (error
-   "Delete this S-Expression and write your own implementation"))
+  "For cons cell QUEEN, given as zero-indexed (COLUMN . ROW) return t if COLUMN
+and ROW are both between 0 and 7 inclusive; otherwise, return nil."
+
+  (defun betweenp (n lower upper)
+    (and (<= lower n)
+         (<= n upper)))
+
+  (let ((col (car queen))
+        (row (cdr queen))
+        (col-row-min 0)
+        (col-row-max 7))
+    (and (betweenp col col-row-min col-row-max)
+         (betweenp row col-row-min col-row-max))))
 
 (defun can-attack-p (white-queen black-queen)
   (error
