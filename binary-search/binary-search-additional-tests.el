e48d7027385590e4f861b827d04ef7cbc3b3ae77
Ryan Matlock <ryan.matlock@gmail.com>
2023-05-24 10:26:30 -0700

    ✅ implement array-bisect function

--- binary-search/binary-search-additional-tests.el
+++ binary-search/binary-search-additional-tests.el
@@ -7,13 +7,16 @@
 
 (load-file "binary-search.el")
 (declare-function bs--safe-1+ "binary-search.el" (value))
-(declare-function bs--array-bisect "binary-search.el" (seq))
+(declare-function bs--array-bisect "binary-search.el" (arr))
 
 (ert-deftest safe-1+-integer ()
   (should (= 2 (bs--safe-1+ 1))))
 
 (ert-deftest safe-1+-nil ()
   (should-not (bs--safe-1+ nil)))
+
+(ert-deftest array-bisect-requires-array ()
+  (should-error (bs--array-bisect '(:foo :bar))))
 
 ;; use car-safe/cdr-safe to safely unpack a bisected array
 (ert-deftest array-bisect-empty-array ()
@@ -25,9 +28,13 @@
 (ert-deftest array-bisect-two-elements ()
   (should (equal '([:foo] . [:bar]) (bs--array-bisect [:foo :bar]))))
 
+;; always favor the left/car side for array of odd length
 (ert-deftest array-bisect-three-elements ()
   (should (equal '([:foo :bar] . [:baz])
                  (bs--array-bisect [:foo :bar :baz]))))
+
+(ert-deftest array-bisect-string ()
+  (should (equal '("foo" . "bar") (bs--array-bisect "foobar"))))
 
 (provide 'binary-search-additional-tests)
 ;;; binary-search-additional-tests.el ends here
--- binary-search/binary-search.el
+++ binary-search/binary-search.el
@@ -5,10 +5,24 @@
 ;;; Code:
 
 
+(require 'cl)
+
 (defun bs--safe-1+ (value)
   "If VALUE is a number, add 1 to VALUE; otherwise, return nil."
   (when (numberp value)
-      (1+ value)))
+    (1+ value)))
+
+(defun bs--array-bisect (arr)
+  (unless (arrayp arr)
+    (error "ARR must be an array."))
+  (let ((len (length arr)))
+    (cond ((= len 0) nil)
+          ((= len 1) (cons arr nil))
+          (t (let ((middle-index (if (evenp len)
+                                     (/ len 2)
+                                   (1+ (/ len 2)))))
+               (cons (seq-subseq arr 0 middle-index)
+                     (seq-subseq arr middle-index)))))))
 
 (defun find-binary (array value)
   (error
