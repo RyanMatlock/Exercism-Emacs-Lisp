7795a85c2654b66d57804568615f009462241191
Ryan Matlock <ryan.matlock@gmail.com>
2023-05-23 18:58:53 -0700

    :recycle: switch to more idiomatic boundary checking

--- queen-attack/queen-attack.el
+++ queen-attack/queen-attack.el
@@ -8,17 +8,12 @@
 (defun valid-position-p (queen)
   "For cons cell QUEEN, given as zero-indexed (COLUMN . ROW) return t if COLUMN
 and ROW are both between 0 and 7 inclusive; otherwise, return nil."
-
-  (defun betweenp (n lower upper)
-    (and (<= lower n)
-         (<= n upper)))
-
   (let ((col (car queen))
         (row (cdr queen))
-        (col-row-min 0)
-        (col-row-max 7))
-    (and (betweenp col col-row-min col-row-max)
-         (betweenp row col-row-min col-row-max))))
+        (min-val 0)
+        (max-val 7))
+    (and (<= min-val col max-val)
+         (<= min-val row max-val))))
 
 (defun qa--diagonalp (pos1 pos2)
     "Return t if the absolute value of the difference in the `car' and `cdr' of
