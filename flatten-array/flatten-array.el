bce27c28e5f4243b224c4b12c772456aabefb892
Ryan Matlock <ryan.matlock@gmail.com>
2023-05-25 20:11:14 -0700

    :construction: flatten singly nested lists

--- flatten-array/flatten-array.el
+++ flatten-array/flatten-array.el
@@ -6,8 +6,19 @@
 
 
 (defun list-flatten (list)
-  (error
-   "Delete this S-Expression and write your own implementation"))
+
+  (defun lf-helper (list acc)
+    (let ((x (car-safe list))
+          (xs (cdr-safe list)))
+      (print (format (concat "list: %s\n"
+                             "acc: %s")
+                     list acc))
+      (cond ((and (null x) (null xs)) (reverse acc))
+            ((and x (atom x)) (lf-helper xs (cons x acc)))
+            ((null x) (lf-helper xs acc))
+            ((listp x) (lf-helper xs (append (lf-helper x '()) acc))))))
+
+  (lf-helper list '()))
 
 
 (provide 'flatten-array)
